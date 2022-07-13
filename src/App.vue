6e95da3652d0b47d5e4f24213028074d7eaef4fd
Aziz_Rakim <azizbekrahimjonov80@gmail.com>
2022-07-13 14:19:18 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
   <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </teleport>
+
+  <div v-if="showModalTwo">
+    <Modal theme="sale" @close="toggleModalTwo">
+      <template v-slot:movies>
+        <a href="#">Watch now</a>
+        <a href="#">Watch later</a>
+      </template>
+      <h1>{{ modalTitle }}</h1>
+      <p>{{ modalDesc }}</p>
+    </Modal>
+  </div>
+
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open second modal</button>
 </template>
 
 <script>
-export default {
+import Modal from './components/Modal.vue'
+
+export  default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
-      title: 'My First Vue App'
+      title: 'My First Vue App :)',
+      header: "Sign up for the Giveaway!",
+      text: "Grab your ninja swag for half price!",
+      showModal: false,
+      showModalTwo: false,
+      modalTitle: "Hercules and the Romans",
+      modalDesc: "Icarus heads for Alexandria to research his class project, but crash lands in Rome. There he's mistaken for a god, until Herc and Nemesis track him down."
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 
 </style>
